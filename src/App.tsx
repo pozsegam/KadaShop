4fcc22aaa6d34c300379fb4ec3197e2873be38e6
pozsgaimate <pozsgai.mate@evista.hu>
2022-11-18 22:53:13 +0100

    feat: react router, queryclient

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { fetchProducts } from './api/fetchProducts';
+import { ROUTE } from './navigation/routes';
+import Details from './pages/Details';
+import Home from './pages/Home';
 
 function App() {
-  return <h1 className='text-3xl font-bold'>Hello world!</h1>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={`${ROUTE.PRODUCTS}`} element={<Home />} />
+          <Route path={`${ROUTE.PRODUCT_DETAILS}/:id`} element={<Details />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/navigation/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTE = {
+  PRODUCTS: "/",
+  PRODUCT_DETAILS: "/details",
+};
