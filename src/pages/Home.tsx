6b205749ef0cef1102bf6e4986478b90574ebfdc
pozsgaimate <pozsgai.mate@evista.hu>
2022-11-19 10:45:03 +0100

    feat: grid styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,20 +8,29 @@ const Home = () => {
   return isLoading ? (
     <Loading />
   ) : (
-    <div className="container">
-      {data?.products?.map((item: any) => {
-        return (
-          <ProductCard
-            key={item.id}
-            id={item.id}
-            discountPercentage={item.discountPercentage}
-            price={item.price}
-            description={item.description}
-            thumbnail={item.thumbnail}
-            name={item.title}
-          />
-        );
-      })}
+    <div className="container m-auto">
+      <header>
+        <h1 className="flex justify-center text-5xl font-bold m-12 text-center">
+          See Products
+        </h1>
+      </header>
+      <main>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 between:grid-cols-3 lg:grid-cols-4">
+          {data?.products?.map((item: any) => {
+            return (
+              <ProductCard
+                key={item.id}
+                id={item.id}
+                discountPercentage={item.discountPercentage}
+                price={item.price}
+                description={item.description}
+                thumbnail={item.thumbnail}
+                name={item.title}
+              />
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 };
