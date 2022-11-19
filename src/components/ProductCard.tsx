593cfb3f17e47090cf0fe7b82797e1d8b3987b8b
pozsgaimate <pozsgai.mate@evista.hu>
2022-11-19 23:09:08 +0100

    fix: moved action to button on product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -22,9 +22,7 @@ const ProductCard: React.FC<CardProps> = ({
   const navigate = useNavigate();
 
   return (
-    <div
-      className="container w-72 h-80 mx-auto bg-white py-2.5 px-3  rounded-md border border-[#DBDBDB]"
-      onClick={() => navigate(`${ROUTE.PRODUCT_DETAILS}/${id}`)}>
+    <div className="container w-72 h-80 mx-auto bg-white py-2.5 px-3  rounded-md border border-[#DBDBDB]">
       <div className="container relative overflow-hidden h-36 rounded-md">
         <span className="absolute right-2 top-2 text-white text-sm font-semibold bg-main-purple px-4 py-1 rounded-2xl">
           -{discountPercentage}%
@@ -42,7 +40,9 @@ const ProductCard: React.FC<CardProps> = ({
         <h1 className="text-dark-text text-2xl font-semibold">{price}$</h1>
       </div>
       <p className="mb-3.5">{description.substring(0, 50)}</p>
-      <button className=" text-white bg-black text-base font-semibold py-3 w-full rounded-3xl">
+      <button
+        onClick={() => navigate(`${ROUTE.PRODUCT_DETAILS}/${id}`)}
+        className=" text-white bg-black text-base font-semibold py-3 w-full rounded-3xl">
         See details
       </button>
     </div>
