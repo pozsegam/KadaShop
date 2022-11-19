3cdac9f84e43aa4f15e99a0932932b912bcc7458
pozsgaimate <pozsgai.mate@evista.hu>
2022-11-19 10:44:28 +0100

    feat: adding custom screen size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-text': '#323232',
+        'main-purple': '#6100FF',
+      },
+      screens: {
+        between: '1024px',
+        lg: '1280px',
+      },
+    },
   },
   plugins: [],
 };
