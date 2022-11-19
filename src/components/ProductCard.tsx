33134c0dcff04c2b621e97b7a9f5fc936b3bd4da
pozsgaimate <pozsgai.mate@evista.hu>
2022-11-19 19:58:38 +0100

    chore: removed unnecessary comments

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -25,9 +25,7 @@ const ProductCard: React.FC<CardProps> = ({
     <div
       className="container w-72 h-80 mx-auto bg-white py-2.5 px-3  rounded-md border border-[#DBDBDB]"
       onClick={() => navigate(`${ROUTE.PRODUCT_DETAILS}/${id}`)}>
-      {/* IMG CONTAINER*/}
       <div className="container relative overflow-hidden h-36 rounded-md">
-        {/* DISCOUNT CHIP */}
         <span className="absolute right-2 top-2 text-white text-sm font-semibold bg-main-purple px-4 py-1 rounded-2xl">
           -{discountPercentage}%
         </span>
@@ -37,16 +35,13 @@ const ProductCard: React.FC<CardProps> = ({
           alt={name}
         />
       </div>
-      {/* PRICE AND TITLE */}
       <div className="container items-baseline flex justify-between mt-2">
         <h1 className="text-dark-text text-xl font-semibold">
           {name.length > 15 ? `${name.substring(0, 15)}...` : name}
         </h1>
         <h1 className="text-dark-text text-2xl font-semibold">{price}$</h1>
       </div>
-      {/* DESCRIPTION */}
       <p className="mb-3.5">{description.substring(0, 50)}</p>
-      {/* See Details Button */}
       <button className=" text-white bg-black text-base font-semibold py-3 w-full rounded-3xl">
         See details
       </button>
