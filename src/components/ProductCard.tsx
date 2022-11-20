3e673ede6b1273a5a592f2fbbeac2effe8c1cf9d
pozsgaimate <pozsgai.mate@evista.hu>
2022-11-20 11:25:58 +0100

    feat: custom hover effect for product card button

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -14,7 +14,9 @@ const ProductCard: React.FC<Product> = ({
   const navigate = useNavigate();
 
   return (
-    <div className="container w-72 h-80 mx-auto bg-white py-2.5 px-3  rounded-md border border-[#DBDBDB]">
+    <div
+      onClick={() => navigate(`${ROUTE.PRODUCT_DETAILS}/${id}`)}
+      className="container w-72 h-80 mx-auto bg-white py-2.5 px-3  rounded-md border border-[#DBDBDB] cursor-pointer">
       <div className="container relative overflow-hidden h-36 rounded-md">
         <span className="absolute right-2 top-2 text-white text-sm font-semibold bg-main-purple px-4 py-1 rounded-2xl">
           -{discountPercentage}%
@@ -32,9 +34,7 @@ const ProductCard: React.FC<Product> = ({
         <h1 className="text-dark-text text-2xl font-semibold">{price}$</h1>
       </div>
       <p className="mb-3.5">{description.substring(0, 50)}</p>
-      <button
-        onClick={() => navigate(`${ROUTE.PRODUCT_DETAILS}/${id}`)}
-        className=" text-white bg-black text-base font-semibold py-3 w-full rounded-3xl">
+      <button className="text-white text-base font-semibold py-3 w-full rounded-3xl transition ease-in-out delay-150  bg-black hover:-translate-y-1 hover:scale-105 hover:bg-main-purple duration-300">
         See details
       </button>
     </div>
