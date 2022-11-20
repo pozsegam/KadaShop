812119b1120ad0fc6381aaa47a7b3bd136034d6b
pozsgaimate <pozsgai.mate@evista.hu>
2022-11-20 13:51:05 +0100

    feat: SEO react async helmet

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -2,6 +2,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
 import { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -14,76 +15,83 @@ const Details = () => {
   const { data } = useProduct(Number(id));
 
   return (
-    <main className="w-screen h-screen">
-      <div className="flex h-full flex-col justify-center items-center lg:flex-row">
-        <section className="mb-20 lg:mr-20 ">
-          <Swiper
-            spaceBetween={20}
-            slidesPerView={1}
-            modules={[Navigation, Pagination]}
-            navigation
-            pagination={{
-              clickable: true,
-            }}
-            className="w-[700px] h-[700px] flex items-center justify-center">
-            {data?.images?.map((img: string) => {
-              return (
-                <SwiperSlide
-                  className="flex justify-center items-center"
-                  key={img}>
-                  <img
-                    className="block w-3/4 h-3/4 object-contain"
-                    key={img}
-                    src={img}
-                    alt={img}
-                  />
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </section>
+    <>
+      <Helmet>
+        <title>{`${data.title} on sale!`} </title>
+        <meta name="description" content={data.description} />
+        <link rel="canonical" href={`/KadaShop/${id}`} />
+      </Helmet>
+      <main className="w-screen h-screen">
+        <div className="flex h-full flex-col justify-center items-center lg:flex-row">
+          <section className="mb-20 lg:mr-20 ">
+            <Swiper
+              spaceBetween={20}
+              slidesPerView={1}
+              modules={[Navigation, Pagination]}
+              navigation
+              pagination={{
+                clickable: true,
+              }}
+              className="w-[700px] h-[700px] flex items-center justify-center">
+              {data?.images?.map((img: string) => {
+                return (
+                  <SwiperSlide
+                    className="flex justify-center items-center"
+                    key={img}>
+                    <img
+                      className="block w-3/4 h-3/4 object-contain"
+                      key={img}
+                      src={img}
+                      alt={img}
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </section>
 
-        <section>
-          <div className="flex justify-between items-center">
-            <h1 className="text-dark-text text-5xl font-semibold w-72 overflow break-words">
-              {data.title}
-            </h1>
-            <div className="flex items-center">
-              <Rating rating={Number(data.rating)} />
-              <span className="text-2xl font-bold ml-2">{data.rating}</span>
+          <section>
+            <div className="flex justify-between items-center">
+              <h1 className="text-dark-text text-5xl font-semibold w-72 overflow break-words">
+                {data.title}
+              </h1>
+              <div className="flex items-center">
+                <Rating rating={Number(data.rating)} />
+                <span className="text-2xl font-bold ml-2">{data.rating}</span>
+              </div>
             </div>
-          </div>
-          <p className="font-medium text-xl w-[400px] my-5">
-            {data.description}
-          </p>
-          <ul>
-            <li className="text-item-color text-xl font-medium mb-2">
-              Stock: {data.stock}
-            </li>
-            <li className="text-item-color text-xl font-medium mb-2">
-              Brand: {data.brand}
-            </li>
-            <li className="text-item-color text-xl font-medium mb-2">
-              Category: {data.category}
-            </li>
-          </ul>
-          <div className="mt-6 mb-2">
-            <span className="text-white text-xl font-semibold bg-main-purple px-6 py-2 rounded-3xl">
-              -{data.discountPercentage}%
-            </span>
-          </div>
+            <p className="font-medium text-xl w-[400px] my-5">
+              {data.description}
+            </p>
+            <ul>
+              <li className="text-item-color text-xl font-medium mb-2">
+                Stock: {data.stock}
+              </li>
+              <li className="text-item-color text-xl font-medium mb-2">
+                Brand: {data.brand}
+              </li>
+              <li className="text-item-color text-xl font-medium mb-2">
+                Category: {data.category}
+              </li>
+            </ul>
+            <div className="mt-6 mb-2">
+              <span className="text-white text-xl font-semibold bg-main-purple px-6 py-2 rounded-3xl">
+                -{data.discountPercentage}%
+              </span>
+            </div>
 
-          <div className="flex items-center justify-between">
-            <h1 className="text-dark-text text-[64px] font-semibold">
-              {data.price}$
-            </h1>
-            <button className="bg-black text-white text-2xl font-semibold px-14 h-14 rounded-full">
-              Add to cart
-            </button>
-          </div>
-        </section>
-      </div>
-    </main>
+            <div className="flex items-center justify-between">
+              <h1 className="text-dark-text text-[64px] font-semibold">
+                {data.price}$
+              </h1>
+              <button className="bg-black text-white text-2xl font-semibold px-14 h-14 rounded-full">
+                Add to cart
+              </button>
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useRef } from 'react';
+import { Helmet } from 'react-helmet-async';
 
 import ProductCard from '../components/ProductCard';
 import { useProducts } from '../hooks/useProducts';
@@ -9,33 +10,44 @@ const Home = () => {
   const { data, isError } = useProducts(observerElem);
 
   return (
-    <div className="container m-auto">
-      <header>
-        <h1 className="flex justify-center text-5xl font-bold m-12 text-center">
-          See Products
-        </h1>
-      </header>
-      <main>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 between:grid-cols-3 lg:grid-cols-4">
-          {data?.pages.map((page): any =>
-            page.products.map((item: Product) => {
-              return (
-                <ProductCard
-                  key={item.id}
-                  id={item.id}
-                  discountPercentage={item.discountPercentage}
-                  price={item.price}
-                  description={item.description}
-                  thumbnail={item.thumbnail}
-                  title={item.title}
-                />
-              );
-            }),
-          )}
-        </div>
-      </main>
-      <div className="w-full" ref={observerElem}></div>
-    </div>
+    <>
+      <Helmet>
+        <title>Shop</title>
+        <meta
+          name="description"
+          content="Do you want to buy the perfect Christmas present, but dont have and idea for present? Search no more! Shop our products with a huge discount before Christmas."
+        />
+        <link rel="canonical" href="/KadaShop" />
+      </Helmet>
+
+      <div className="container m-auto">
+        <header>
+          <h1 className="flex justify-center text-5xl font-bold m-12 text-center">
+            See Products
+          </h1>
+        </header>
+        <main>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 between:grid-cols-3 lg:grid-cols-4">
+            {data?.pages.map((page): any =>
+              page.products.map((item: Product) => {
+                return (
+                  <ProductCard
+                    key={item.id}
+                    id={item.id}
+                    discountPercentage={item.discountPercentage}
+                    price={item.price}
+                    description={item.description}
+                    thumbnail={item.thumbnail}
+                    title={item.title}
+                  />
+                );
+              }),
+            )}
+          </div>
+        </main>
+        <div className="w-full" ref={observerElem}></div>
+      </div>
+    </>
   );
 };
 export default Home;
